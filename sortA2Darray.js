08a2890aa786ecd3f09799ff3bba0324b994e407
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-28 11:39:28 -0600

    passed 13

--- sortA2Darray.js
+++ sortA2Darray.js
@@ -1,18 +1,18 @@
-var arr = [["Pablo", "Enrique", "Josh", "Juan", "Gonzalo"], ["Michael", "Alexander", "Mikel", "Ariel"], ["John", "Julia", "Brad", "George"]];
-//     Test.expect(Array.isArray(namesSorter(arr)), "Is not returning an array");
+// var arr = [["Pablo", "Enrique", "Josh", "Juan", "Gonzalo"], ["Michael", "Alexander", "Mikel", "Ariel"], ["John", "Julia", "Brad", "George"]];
+    // Test.expect(Array.isArray(namesSorter(arr)), "Is not returning an array");
 
-//
-//   it ("returns ['Brad', 'Juan'] when array is [['Juan'], ['Brad']]", function(){
-//     Test.assertDeepEquals(namesSorter([['Juan'], ['Brad']]), ['Brad', 'Juan']);
-//   });
-//
-//   it ("returns [ 'Brad', 'Juan', 'Ariel', 'Julia', 'Michael', 'Alexander' ] when array is [['Juan','Ariel','Julia'], ['Brad','Michael','Alexander']]", function(){
-//     Test.assertDeepEquals(namesSorter([['Juan','Ariel','Julia'], ['Brad','Michael','Alexander']]), [ 'Brad', 'Juan', 'Ariel', 'Julia', 'Michael', 'Alexander' ]);
-//   });
-//
-//   it ("returns [ 'Brad','John','Josh','Juan','Ariel','Julia','Mikel','Pablo','George','Enrique','Gonzalo','Michael','Alexander' ] when array is [['Pablo', 'Enrique', 'Josh', 'Juan', 'Gonzalo'], ['Michael', 'Alexander', 'Mikel', 'Ariel'], ['John', 'Julia', 'Brad', 'George']]", function(){
-//     Test.assertDeepEquals(namesSorter([["Pablo", "Enrique", "Josh", "Juan", "Gonzalo"], ["Michael", "Alexander", "Mikel", "Ariel"], ["John", "Julia", "Brad", "George"]]), [ 'Brad','John','Josh','Juan','Ariel','Julia','Mikel','Pablo','George','Enrique','Gonzalo','Michael','Alexander' ]);
-//   });
+// let arr = [['Juan'], ['Brad']]
+  // it ("returns ['Brad', 'Juan'] when array is [['Juan'], ['Brad']]", function(){
+  //   Test.assertDeepEquals(namesSorter([['Juan'], ['Brad']]), ['Brad', 'Juan']);
+  // });
+// let arr = [['Juan','Ariel','Julia'], ['Brad','Michael','Alexander']]
+  // it ("returns [ 'Brad', 'Juan', 'Ariel', 'Julia', 'Michael', 'Alexander' ] when array is [['Juan','Ariel','Julia'], ['Brad','Michael','Alexander']]", function(){
+  //   Test.assertDeepEquals(namesSorter([['Juan','Ariel','Julia'], ['Brad','Michael','Alexander']]), [ 'Brad', 'Juan', 'Ariel', 'Julia', 'Michael', 'Alexander' ]);
+  // });
+let arr = [['Pablo', 'Enrique', 'Josh', 'Juan', 'Gonzalo'], ['Michael', 'Alexander', 'Mikel', 'Ariel'], ['John', 'Julia', 'Brad', 'George']]
+  // it ("returns [ 'Brad','John','Josh','Juan','Ariel','Julia','Mikel','Pablo','George','Enrique','Gonzalo','Michael','Alexander' ] when array is [['Pablo', 'Enrique', 'Josh', 'Juan', 'Gonzalo'], ['Michael', 'Alexander', 'Mikel', 'Ariel'], ['John', 'Julia', 'Brad', 'George']]", function(){
+  //   Test.assertDeepEquals(namesSorter([["Pablo", "Enrique", "Josh", "Juan", "Gonzalo"], ["Michael", "Alexander", "Mikel", "Ariel"], ["John", "Julia", "Brad", "George"]]), [ 'Brad','John','Josh','Juan','Ariel','Julia','Mikel','Pablo','George','Enrique','Gonzalo','Michael','Alexander' ]);
+  // });
 
 
 function namesSorter(departmentsArray) {
@@ -27,8 +27,14 @@ function namesSorter(departmentsArray) {
 
 
   });
-
-  newArr = newArr.sort()
+  //ordenar por legh
+  newArr.sort(function(a, b) {
+    return a.length - b.length || // sort by length, if equal then
+      a.localeCompare(b); // sort by dictionary order
+  });
+  // ordernar alfabeticamente
+  // newArr = newArr.sort()
+  console.log(newArr);
   return newArr;
 }
 namesSorter(arr)
